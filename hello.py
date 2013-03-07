1787bd71352480b3107e3c9812f7d983b61357b3
Todd Dow <toddhdow@gmail.com>
2013-03-07 11:24:05 -0500

    Messing around…

---
+++ hello.py
@@ -0,0 +1,9 @@
+import webapp2
+
+class hello(webapp2.RequestHandler):
+  def get(self):
+      self.response.headers['Content-Type'] = 'text/plain'
+      self.response.write('Welcome to my MLB Data Analysis App!')
+
+app = webapp2.WSGIApplication([('/', hello)],
+                              debug=True)
