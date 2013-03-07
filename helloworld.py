8e5c44659b764921ac7bee5f201dc04bc411616d
Todd Dow <toddhdow@gmail.com>
2013-03-07 11:23:34 -0500

    Messing around…

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,9 @@
 import webapp2
 
-class MainPage(webapp2.RequestHandler):
+class hello(webapp2.RequestHandler):
   def get(self):
       self.response.headers['Content-Type'] = 'text/plain'
       self.response.write('Welcome to my MLB Data Analysis App!')
 
-app = webapp2.WSGIApplication([('/', MainPage)],
+app = webapp2.WSGIApplication([('/hello', hello)],
                               debug=True)
