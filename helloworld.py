4ebe8128359731d824c6e118479a7e410530c51b
Todd Dow <toddhdow@gmail.com>
2014-01-22 11:02:25 -0500

    First php build. local server works.

--- helloworld.py
+++
@@ -1,9 +0,0 @@
-import webapp2
-
-class MainPage(webapp2.RequestHandler):
-  def get(self):
-      self.response.headers['Content-Type'] = 'text/plain'
-      self.response.write('Welcome to my MLB Data Analysis App!')
-
-app = webapp2.WSGIApplication([('/', MainPage)],
-                              debug=True)
